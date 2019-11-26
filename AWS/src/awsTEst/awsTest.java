9565626e3988b35480de7c31a344a2a9d5700c8c
rudwn <rudwn@LAPTOP-MKV7A847>
2019-11-26 19:37:07 +0900

    인스턴스생성시 Name 설정 기능 추가, 인스턴스 목록 확인 시 Name 포함 출력 기능 추가 (기능 최종 수정 완료)

--- AWS/src/awsTEst/awsTest.java
+++ AWS/src/awsTEst/awsTest.java
@@ -1,100 +1,24 @@
 package awsTEst;
 
-/*
- * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *  http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.List;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-import com.amazonaws.services.acmpca.model.Tag;
-import com.amazonaws.services.applicationdiscovery.model.CreateTagsRequest;
-import com.amazonaws.services.applicationdiscovery.model.CreateTagsResult;
-import com.amazonaws.services.appstream.model.DeleteImageRequest;
-import com.amazonaws.services.appstream.model.DeleteImageResult;
-import com.amazonaws.services.codedeploy.model.InstanceType;
-import com.amazonaws.services.config.model.Owner;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
 import com.amazonaws.services.ec2.model.*;
 import com.amazonaws.services.identitymanagement.AmazonIdentityManagementClient;
-import com.amazonaws.services.organizations.model.DescribeAccountRequest;
-import com.amazonaws.services.organizations.model.DescribeAccountResult;
 
-/**
- * Welcome to your new AWS Java SDK based project!
- *
- * This class is meant as a starting point for your console-based application
- * that makes one or more calls to the AWS services supported by the Java SDK,
- * such as EC2, SimpleDB, and S3.
- *
- * In order to use the services in this sample, you need:
- *
- * - A valid Amazon Web Services account. You can register for AWS at:
- * https://aws-portal.amazon.com/gp/aws/developer/registration/index.html
- *
- * - Your account's Access Key ID and Secret Access Key:
- * http://aws.amazon.com/security-credentials
- *
- * - A subscription to Amazon EC2. You can sign up for EC2 at:
- * http://aws.amazon.com/ec2/
- *
- * - A subscription to Amazon SimpleDB. You can sign up for Simple DB at:
- * http://aws.amazon.com/simpledb/
- *
- * - A subscription to Amazon S3. You can sign up for S3 at:
- * http://aws.amazon.com/s3/
- */
 public class awsTest {
-
-	static List<String> id_list = new ArrayList<String>();
-
-	/*
-	 * Before running the code: Fill in your AWS access credentials in the provided
-	 * credentials file template, and be sure to move the file to the default
-	 * location (~/.aws/credentials) where the sample code will load the credentials
-	 * from. https://console.aws.amazon.com/iam/home?#security_credential
-	 *
-	 * WARNING: To avoid accidental leakage of your credentials, DO NOT keep the
-	 * credentials file in your source directory.
-	 */
 
 	static AmazonEC2 ec2;
 
-	/**
-	 * The only information needed to create a client are security credentials
-	 * consisting of the AWS Access Key ID and Secret Access Key. All other
-	 * configuration, such as the service endpoints, are performed automatically.
-	 * Client parameters, such as proxies, can be specified in an optional
-	 * ClientConfiguration object when constructing a client.
-	 *
-	 * @see com.amazonaws.auth.BasicAWSCredentials
-	 * @see com.amazonaws.auth.PropertiesCredentials
-	 * @see com.amazonaws.ClientConfiguration
-	 */
 	private static void init() throws Exception {
-
-		/*
-		 * The ProfileCredentialsProvider will return your [default] credential profile
-		 * by reading from the credentials file located at (~/.aws/credentials).
-		 */
 		ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
 		try {
 			credentialsProvider.getCredentials();
@@ -117,27 +41,29 @@ public class awsTest {
 			DescribeInstancesResult response = ec2.describeInstances(request);
 			for (Reservation reservation : response.getReservations()) {
 				for (Instance instance : reservation.getInstances()) {
-					if(instance.getState().getName().equals("terminated")) {//terminated 상태인 instance 예외처리
-						System.out.printf(
-								"[id] %s, " + "[AMI] %s, " + "[type] %s, " + "[state] %10s, " ,
-								instance.getInstanceId(), 
-								instance.getImageId(), 
-								instance.getInstanceType(),
+					if(instance.getTags().isEmpty())
+						System.out.printf("[Name] %12s, ", "null");
+					else {
+						for(Tag tag : instance.getTags()) {
+							System.out.print(String.format("[%s] %12s, ", tag.getKey(), tag.getValue()));
+						}
+					}
+					
+					
+
+					if (instance.getState().getName().equals("terminated")) {// terminated 상태인 instance 예외처리
+						System.out.printf("[id] %s, " + "[AMI] %s, " + "[type] %s, " + "[state] %10s, ",
+								instance.getInstanceId(), instance.getImageId(), instance.getInstanceType(),
 								instance.getState().getName());
 						continue;
 					}
+					
 					System.out.printf(
 							"[id] %s, " + "[AMI] %s, " + "[type] %s, " + "[state] %10s, " + "[key pair name] %10s, "
 									+ "[security group] %15s, " + "[monitoring state] %s",
-							instance.getInstanceId(), 
-							instance.getImageId(), 
-							instance.getInstanceType(),
-							instance.getState().getName(), 
-							instance.getKeyName(),
-							instance.getSecurityGroups().get(0).getGroupName(), 
-							//instance.getSecurityGroups().toString(),
-							instance.getMonitoring().getState());
-					//id_list.add(instance.getInstanceId());
+							instance.getInstanceId(), instance.getImageId(), instance.getInstanceType(),
+							instance.getState().getName(), instance.getKeyName(),
+							instance.getSecurityGroups().get(0).getGroupName(), instance.getMonitoring().getState());
 				}
 				System.out.println();
 
@@ -152,11 +78,6 @@ public class awsTest {
 
 	// instance 시작
 	public static void startInstance() {
-		System.out.print("id list : ");
-		for (int i = 0; i < id_list.size(); i++) {
-			System.out.format("%s ", id_list.get(i));
-		}
-		System.out.println();
 		Scanner input_id = new Scanner(System.in);
 		String instance_id;
 		System.out.print("Enter instance id : ");
@@ -172,7 +93,6 @@ public class awsTest {
 
 		if (!dry_response.isSuccessful()) {
 			System.out.printf("Failed dry run to start instance %s", instance_id);
-			// throw dry_response.getDryRunResponse();
 		}
 
 		StartInstancesRequest request = new StartInstancesRequest().withInstanceIds(instance_id);
@@ -187,8 +107,6 @@ public class awsTest {
 		String instance_id;
 		System.out.print("Enter instance id : ");
 		instance_id = input_id.nextLine();
-
-		// final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
 
 		DryRunSupportedRequest<StopInstancesRequest> dry_request = () -> {
 			StopInstancesRequest request = new StopInstancesRequest().withInstanceIds(instance_id);
@@ -216,8 +134,6 @@ public class awsTest {
 		System.out.print("Enter instance id : ");
 		instance_id = input_id.nextLine();
 
-		final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
-
 		RebootInstancesRequest request = new RebootInstancesRequest().withInstanceIds(instance_id);
 
 		RebootInstancesResult response = ec2.rebootInstances(request);
@@ -228,14 +144,14 @@ public class awsTest {
 	public static void imageList() {
 		AmazonIdentityManagementClient iamClient = new AmazonIdentityManagementClient();
 		String accountNumber = iamClient.getUser().getUser().getArn().split(":")[4];
-		// System.out.print(accountNumber);
+
 		DescribeImagesRequest request = new DescribeImagesRequest().withOwners(accountNumber);
 		DescribeImagesResult response = ec2.describeImages(request);
 		for (Image reservation : response.getImages()) {
-			System.out.printf("[ImageID] %s, [State] %s, [Owner] %s, [Name] %s", reservation.getImageId(), 
+			System.out.printf("[ImageID] %s, [State] %s, [Owner] %s, [Name] %s", reservation.getImageId(),
 					reservation.getState(), reservation.getOwnerId(), reservation.getName());
 			System.out.println();
-			
+
 		}
 	}
 
@@ -244,6 +160,7 @@ public class awsTest {
 		String image_id;
 		String keypair_name;
 		String security_group_name;
+		String instance_name;
 		
 		System.out.print("Enter image id : ");
 		image_id = input.nextLine();
@@ -251,17 +168,22 @@ public class awsTest {
 		keypair_name = input.nextLine();
 		System.out.print("Enter security group name : ");
 		security_group_name = input.nextLine();
+		System.out.print("Enter instance name you want : ");
+		instance_name = input.nextLine();
 		
 		RunInstancesRequest run_request = new RunInstancesRequest().withImageId(image_id)
 				.withInstanceType(com.amazonaws.services.ec2.model.InstanceType.T2Micro).withMaxCount(1).withMinCount(1)
-				.withKeyName(keypair_name)
-				.withSecurityGroups(security_group_name);
+				.withKeyName(keypair_name).withSecurityGroups(security_group_name);
 
 		RunInstancesResult run_response = ec2.runInstances(run_request);
+		Instance instance = run_response.getReservation().getInstances().get(0);
+		String instance_id = instance.getInstanceId();
 
-		String reservation_id = run_response.getReservation().getInstances().get(0).getInstanceId();
+		CreateTagsRequest createTagsRequest = new CreateTagsRequest().withResources(instance.getInstanceId())
+				.withTags(new Tag("Name", instance_name));
+		ec2.createTags(createTagsRequest);
 
-		System.out.printf("Successfully started EC2 instance %s based on AMI %s", reservation_id, image_id);
+		System.out.printf("Successfully started EC2 instance [Name] %s, [Id] %s, [Base AMI] %s", instance_name, instance_id, image_id);
 	}
 
 	public static void available_zones() {
@@ -338,112 +260,97 @@ public class awsTest {
 	static void securitygroupList() {
 		DescribeSecurityGroupsRequest request = new DescribeSecurityGroupsRequest();
 
-	        DescribeSecurityGroupsResult response = ec2.describeSecurityGroups(request);
+		DescribeSecurityGroupsResult response = ec2.describeSecurityGroups(request);
 
-	        for(SecurityGroup group : response.getSecurityGroups()) {
-	            System.out.printf("[security group id] %20s, " + "[security group name] %15s ", group.getGroupId(), group.getGroupName());
-	            System.out.println();
-	        }
+		for (SecurityGroup group : response.getSecurityGroups()) {
+			System.out.printf("[security group id] %20s, " + "[security group name] %15s ", group.getGroupId(),
+					group.getGroupName());
+			System.out.println();
+		}
 	}
-	
-	static void createSecuritygroup(){
+
+	static void createSecuritygroup() {
 		Scanner input = new Scanner(System.in);
 		System.out.print("enter security group name : ");
 		String group_name = input.nextLine();
-		
+
 		System.out.print("enter description for security group : ");
 		String group_desc = input.nextLine();
-		
-		
-		CreateSecurityGroupRequest create_request = new CreateSecurityGroupRequest().withGroupName(group_name).withDescription(group_desc);
 
-	        CreateSecurityGroupResult create_response = ec2.createSecurityGroup(create_request);
+		CreateSecurityGroupRequest create_request = new CreateSecurityGroupRequest().withGroupName(group_name)
+				.withDescription(group_desc);
 
-	        System.out.printf("Successfully created security group named %s", group_name);
+		CreateSecurityGroupResult create_response = ec2.createSecurityGroup(create_request);
 
-	        IpRange ip_range = new IpRange()
-	            .withCidrIp("0.0.0.0/0");
+		System.out.printf("Successfully created security group named %s", group_name);
 
-	        IpPermission ip_perm = new IpPermission()
-	            .withIpProtocol("tcp")
-	            .withToPort(80)
-	            .withFromPort(80)
-	            .withIpv4Ranges(ip_range);
+		IpRange ip_range = new IpRange().withCidrIp("0.0.0.0/0");
 
-	        IpPermission ip_perm2 = new IpPermission()
-	            .withIpProtocol("tcp")
-	            .withToPort(22)
-	            .withFromPort(22)
-	            .withIpv4Ranges(ip_range);
+		IpPermission ip_perm = new IpPermission().withIpProtocol("tcp").withToPort(80).withFromPort(80)
+				.withIpv4Ranges(ip_range);
 
-	        AuthorizeSecurityGroupIngressRequest auth_request = new
-	            AuthorizeSecurityGroupIngressRequest()
-	                .withGroupName(group_name)
-	                .withIpPermissions(ip_perm, ip_perm2);
+		IpPermission ip_perm2 = new IpPermission().withIpProtocol("tcp").withToPort(22).withFromPort(22)
+				.withIpv4Ranges(ip_range);
 
-	        AuthorizeSecurityGroupIngressResult auth_response = ec2.authorizeSecurityGroupIngress(auth_request);
+		AuthorizeSecurityGroupIngressRequest auth_request = new AuthorizeSecurityGroupIngressRequest()
+				.withGroupName(group_name).withIpPermissions(ip_perm, ip_perm2);
 
-	        System.out.print("Successfully added ingress policy to security group" + group_name);
-	    
+		AuthorizeSecurityGroupIngressResult auth_response = ec2.authorizeSecurityGroupIngress(auth_request);
+
+		System.out.print("Successfully added ingress policy to security group" + group_name);
+
 	}
-	
-	static void deleteSecuritygroup(){
+
+	static void deleteSecuritygroup() {
 		Scanner input = new Scanner(System.in);
-		System.out.print("enter security group name to be deleted : ");
+		System.out.print("Enter security group name to be deleted : ");
 		String group_name = input.nextLine();
-		
+
 		DeleteSecurityGroupRequest delete_request = new DeleteSecurityGroupRequest().withGroupName(group_name);
 		DeleteSecurityGroupResult delete_response = ec2.deleteSecurityGroup(delete_request);
 		System.out.print(delete_response);
 		System.out.printf("Successfully deleted security group named %s", group_name);
-	    
+
 	}
-	
+
 	public static void deleteInstance() {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter id to be deleted : ");
 		String delete_instance_id = input.nextLine();
-		
-		TerminateInstancesRequest terminateRequest = new TerminateInstancesRequest().withInstanceIds(delete_instance_id);
-	    ec2.terminateInstances(terminateRequest);
+
+		TerminateInstancesRequest terminateRequest = new TerminateInstancesRequest()
+				.withInstanceIds(delete_instance_id);
+		ec2.terminateInstances(terminateRequest);
 	}
-	
+
 	public static void createImage() {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter image name : ");
 		String image_name = input.nextLine();
 
-		
 		System.out.print("Enter source instance id : ");
 		String source_instance_id = input.nextLine();
-		
-		//DeleteSecurityGroupRequest delete_request = new DeleteSecurityGroupRequest().withGroupName(group_name);
-		CreateImageRequest create_request = new CreateImageRequest().withName(image_name).withInstanceId(source_instance_id);
-		//DeleteSecurityGroupResult delete_response = ec2.deleteSecurityGroup(delete_request);
+
+		CreateImageRequest create_request = new CreateImageRequest().withName(image_name)
+				.withInstanceId(source_instance_id);
+
 		CreateImageResult create_response = ec2.createImage(create_request);
-		
+
 		System.out.printf("Successfully created image named %s", image_name);
-		
-		
+
 	}
-	
+
 	public static void deleteImage() {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter image id to be deleted : ");
 		String image_id = input.nextLine();
-		
-		//DeleteSecurityGroupRequest delete_request = new DeleteSecurityGroupRequest().withGroupName(group_name);
-		
-		//DeleteSecurityGroupResult delete_response = ec2.deleteSecurityGroup(delete_request);
-		
-		
-		DeregisterImageRequest deregister_request = new DeregisterImageRequest().withImageId(image_id);		
+
+		DeregisterImageRequest deregister_request = new DeregisterImageRequest().withImageId(image_id);
 		DeregisterImageResult deregister_response = ec2.deregisterImage(deregister_request);
 		System.out.printf("Successfully deleted image");
-		
-		
+
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 
 		System.out.println("===========================================");
@@ -453,9 +360,8 @@ public class awsTest {
 		init();
 
 		Scanner menu = new Scanner(System.in);
-		Scanner id_string = new Scanner(System.in);
 		int number = 0;
-		boolean roop=true;
+		boolean roop = true;
 		while (roop) {
 			System.out.println(" ");
 			System.out.println(" ");
@@ -532,11 +438,11 @@ public class awsTest {
 				deleteImage();
 				break;
 			case 99:
-				roop=false;
+				roop = false;
 				break;
 			}
 		}
 		System.out.print("Bye~");
 	}
-	
+
 }
